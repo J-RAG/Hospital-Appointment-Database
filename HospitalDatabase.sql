9c9c1de32a0e4f0c563021aa0f1c030e45bc0fa6
J-RAG <42630843+J-RAG@users.noreply.github.com>
2021-08-18 15:26:20 +1200

    Create Database
    Create Database
    Drop Database
    Use Database

---
+++ HospitalDatabase.sql
@@ -0,0 +1,14 @@
+--	Drop Database
+/**
+ DROP DATABASE HospitalAppointment;
+ GO
+ **/
+
+--	Create Database
+CREATE DATABASE HospitalAppointment;
+GO
+
+--	Use Database
+USE [HospitalAppointment];
+GO
+
